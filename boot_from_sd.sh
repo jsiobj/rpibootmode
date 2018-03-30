2343aad88fe6b4d1c5729ce8c6dc021c8956121b
jsiobj <js@interactingobjects.com>
2018-03-30 20:19:12 +0000

    Various improvments (error testing...)

--- boot_from_sd.sh
+++ boot_from_sd.sh
@@ -1,27 +1,47 @@
 #!/bin/sh
 
-if [ `id -u` -ne 0 ]; then
+reboot=1
+
+enable_file() {
+  filename=$1
+
+  mv $filename.disable $filename
+  if [ $? -ne 0 ]; then
+    echo WAR Could not "enable" $filename
+    reboot=0
+  else
+    echo INF File $filename "enabled"
+  fi
+}
+
+if [ "`id -u`" -ne "0" ]; then
   echo Must be root to run this script
   exit 1
 fi
 
-mount /dev/mmcblk0p1 /media/sd/boot
-if [ $? -ne 0 ]; then
-  echo Failed mount SD boot fs
+if [ `mount | grep " / " | grep "/dev/sd" | wc -l` -ne 1 ]; then
+  echo ERR Looks like not booted from USB. Stopping
   exit 1
 fi
 
-mv /media/sd/boot/config.txt.disable /media/sd/boot/config.txt
-if [ $? -ne 0 ]; then
-  echo Failed to restore config.txt
-  exit 1
+mount | grep /media/sd/boot >/dev/null 2>&1
+if [ $? -ne 0 ]; then 
+  echo INF Mounting /dev/mmcblk0p1 on /media/sd/boot
+  mount /dev/mmcblk0p1 /media/sd/boot
+  if [ $? -ne 0 ]; then
+    echo ERR Failed mount SD boot fs
+    exit 1
+  fi
+else
+  echo INF /media/boot/sd already mounted
 fi
 
-mv /media/sd/boot/start.elf.disable /media/sd/boot/start.elf
-if [ $? -ne 0 ]; then
-  echo Failed to restore start.elf
-  exit 1
+enable_file /boot/config.txt
+enable_file /boot/start.elf
+
+if [ $reboot -eq 1 ]; then
+  echo INF Rebooting
+  #reboot
+else
+  echo INF Not rebooting
 fi
-
-reboot
-
--- boot_from_usb.sh
+++ boot_from_usb.sh
@@ -1,6 +1,35 @@
 #!/bin/sh
 
-mv /boot/config.txt /boot/config.txt.disable
-mv /boot/start.elf /boot/start.elf.disable
+reboot=1
 
-reboot
+disable_file() {
+  filename=$1
+
+  mv $filename $filename.disable
+  if [ $? -ne 0 ]; then
+    echo WAR Could not "disable" $filename
+    reboot=0
+  else 
+    echo INF File $filename "disabled"
+  fi
+}
+
+if [ `id -u` -ne 0 ]; then
+  echo ERR Must be root to run this script
+  exit 1
+fi
+
+if [ `mount | grep " / " | grep "/dev/sd" | wc -l` -eq 1 ]; then
+  echo WAR Looks like already booted on USB
+  reboot=0
+fi
+
+disable_file /boot/config.txt 
+disable_file /boot/start.elf
+
+if [ $reboot -eq 1 ]; then
+  echo INF Rebooting
+  reboot
+else
+  echo INF Not rebooting
+fi
